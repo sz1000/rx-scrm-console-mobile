699313d554d1d65566c63c545112b23868b0d922
yuanjingyun <yuanjingyun@ruixin.tech>
2021-08-11 14:46:27 +0800

    yjy

--- scrm-app/src/views/talkTool/ChannelConfiguration.vue
+++ scrm-app/src/views/talkTool/ChannelConfiguration.vue
@@ -1,8 +1,21 @@
 <template>
   <div>
     <div class="topInfo">
-        <div class="flag">拓客管理</div>
-    </div>
+      <div class="top_left_box">
+        <div class="blue-tap"></div>
+        <div class="flag">全部渠道</div>
+        <div class='editButton'
+                @click="showCompany(1)">
+              <i class="el-icon-edit"></i>
+            新增
+          </div>
+
+      </div>
+      <div class="top_right_box">
+            
+      </div>
+   </div>
+
     渠道配置
   </div>
 </template>
@@ -11,14 +24,47 @@
 <style lang ="less">
   .ChannelConfiguration{
     .topInfo{
-      height: 88px;
-      width: 750px;
+      height: 115px;
       background: #ffffff;
-      .flag{
-        margin-left: 312px;
-        margin-top: 20px;
-        height: 45px;
-        width: 128px;
+      .top_left_box{
+        display: flex;
+        /* display : inline-block; */
+        .blue-tap{
+          height: 28px;
+          width: 8px;
+          background: #4168F6;
+          margin-left: 24px;
+          margin-top: 44px;
+        }
+        .flag{
+          margin-top: 44px;
+          height: 45px;
+          width: 128px;
+          text-align: center;
+        }
+        .editButton {
+          right: 0px;
+            color: #838a9d;
+            width: 152px;
+            height: 68px;
+            border-radius: 8px;
+            border: 2px solid #d9dae4;
+            text-align: center;
+            line-height: 68px;
+            margin-top: 24px;
+            margin-right:0;
+            span {
+              display: inline-block;
+            }
+            img {
+              display: inline-block;
+              vertical-align: middle;
+              margin: -10px 10px 0 0;
+              margin-right: 5px;
+              width: 28px;
+              height: 28px;
+            }
+          }
       }
     }
   }
