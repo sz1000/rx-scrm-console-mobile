743e06df660501f0a9f271ae86d39bf06dd61216
linshaojie <linshaojie@ruixin.tech>
2021-08-11 14:46:43 +0800

    lsj

--- scrm-app/src/views/talkTool/EnterpriseCode.vue
+++ scrm-app/src/views/talkTool/EnterpriseCode.vue
@@ -28,11 +28,11 @@
           <span>{{item.livename}}</span>
         </div>
         <div class="editBtn">
-          <span>
+          <span @click="editBtn(item)">
             <van-icon name="edit" />
             编辑
           </span>
-          <span>
+          <span @click="deleteBtn(item)">
             <van-icon name="delete-o" />
             删除
           </span>
@@ -42,13 +42,15 @@
         <div class="leftCode">
           <img :src="item.address"
                alt="">
-          <div class="shareCode">
+          <div class="shareCode"
+               @click="sendCode(item,index)">
             <span> <img src="../../images/send.png"
                    alt=""></span>
             发送二维码
           </div>
         </div>
-        <div class="rightInfo">
+        <div class="rightInfo"
+             @click="checkDetail(item,index)">
           <div class="rowText">
             <span>渠道:</span>
             <span>渠道</span>
@@ -72,8 +74,161 @@
       <van-action-sheet v-model="show"
                         :title="titleName">
         <div class="content">
+          <div class="addForm"
+               v-if="isShowDialog=='1'">
+            <el-form ref="form"
+                     :model="addForm"
+                     label-position='right'>
+              <el-form-item label="活码名称:"
+                            prop="name"
+                            :rules="[ { required: true, message: '请输入活码名称'}]">
+                <el-input v-model="addForm.name"
+                          placeholder="请输入"
+                          maxlength="12"
+                          show-word-limit></el-input>
+              </el-form-item>
 
-          <div class="buttonWarp">
+              <el-form-item label="使用员工:"
+                            prop="userNo"
+                            :rules="[ { required: true, message: '请选择'}]">
+                <el-select v-model="addForm.usreNo"
+                           placeholder="请选择使用员工，可多选"
+                           multiple
+                           collapse-tags
+                           @change="changeUsre"
+                           clearable>
+                  <el-option v-for="item in usreList"
+                             :key="item.value"
+                             :label="item.name"
+                             :value="item.type">
+                  </el-option>
+                </el-select>
+              </el-form-item>
+
+              <el-form-item label="添加设置:">
+                <el-checkbox v-model="addForm.checked"
+                             true-label='1'
+                             false-label='0'
+                             @change="checkChange">设置添加时无需经过确认自动成为好友</el-checkbox>
+              </el-form-item>
+
+              <el-form-item label="渠道:">
+                <el-select v-model="addForm.source"
+                           placeholder="请选择"
+                           @change="changeChannel"
+                           clearable>
+                  <el-option v-for="item in channelList"
+                             :key="item.value"
+                             :label="item.name"
+                             :value="item.type">
+                  </el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="备注:"
+                            class="textareaInput">
+                <el-input type="textarea"
+                          v-model="addForm.remark"
+                          placeholder="请输入文字(不得超过200个字符)"
+                          maxlength="200"
+                          show-word-limit></el-input>
+              </el-form-item>
+            </el-form>
+          </div>
+          <div class="addForm"
+               v-if="isShowDialog=='2'">
+            <el-form ref="form"
+                     :model="editForm"
+                     label-position='right'>
+              <el-form-item label="创建人员:">
+                <span class="editText">{{editForm.creatBy}}</span>
+              </el-form-item>
+              <el-form-item label="创建时间:">
+                <span class="editText">{{formatDate(editForm.creatTime,'yyyy-MM-dd')}}</span>
+              </el-form-item>
+              <el-form-item label="活码名称:"
+                            prop="name"
+                            :rules="[ { required: true, message: '请输入活码名称'}]">
+                <el-input v-model="editForm.name"
+                          placeholder="请输入"
+                          maxlength="12"
+                          show-word-limit></el-input>
+              </el-form-item>
+
+              <el-form-item label="使用员工:"
+                            prop="userNo"
+                            :rules="[ { required: true, message: '请选择'}]">
+                <el-select v-model="editForm.usreNo"
+                           placeholder="请选择使用员工，可多选"
+                           multiple
+                           collapse-tags
+                           @change="changeUsre"
+                           clearable>
+                  <el-option v-for="item in usreList"
+                             :key="item.value"
+                             :label="item.name"
+                             :value="item.type">
+                  </el-option>
+                </el-select>
+              </el-form-item>
+
+              <el-form-item label="添加设置:">
+                <el-checkbox v-model="editForm.checked"
+                             true-label='1'
+                             false-label='0'
+                             @change="checkChange">设置添加时无需经过确认自动成为好友</el-checkbox>
+              </el-form-item>
+
+              <el-form-item label="渠道:">
+                <el-select v-model="editForm.source"
+                           placeholder="请选择"
+                           @change="changeChannel"
+                           clearable>
+                  <el-option v-for="item in channelList"
+                             :key="item.value"
+                             :label="item.name"
+                             :value="item.type">
+                  </el-option>
+                </el-select>
+              </el-form-item>
+              <el-form-item label="备注:"
+                            class="textareaInput">
+                <el-input type="textarea"
+                          v-model="editForm.remark"
+                          placeholder="请输入文字(不得超过200个字符)"
+                          maxlength="200"
+                          show-word-limit></el-input>
+              </el-form-item>
+            </el-form>
+          </div>
+          <div class="codeDetail"
+               v-if="isShowDialog=='3'">
+            <el-form label-position="right"
+                     :model="detailForm">
+              <el-form-item label="活码展示:"
+                            class="codeImg">
+                <img :src="detailForm.address"
+                     alt=""
+                     style="width:120px;height:120px" />
+              </el-form-item>
+              <el-form-item label="使用员工:">
+                <span>{{ detailForm.userNames }}</span>
+              </el-form-item>
+              <el-form-item label="创建人:">
+                <span>{{ detailForm.createBy }}</span>
+              </el-form-item>
+              <el-form-item label="创建时间:">
+                <span>{{ formatDate( detailForm.createTime ,'yyyy-MM-dd') }}</span>
+              </el-form-item>
+              <el-form-item label="渠道:">
+                <span>{{ detailForm.chName }}</span>
+              </el-form-item>
+              <el-form-item label="备注:">
+                <span>{{ detailForm.remark }}</span>
+              </el-form-item>
+            </el-form>
+          </div>
+          <div class="buttonWarp"
+               v-if="isShowDialog !==3">
             <span class="cancel"
                   @click="closeDialog(isShowDialog)">取消</span>
             <span class="save"
@@ -85,6 +240,7 @@
   </div>
 </template>
 <script>
+import { formatDate } from '../../utils/tool'
 export default {
   data() {
     return {
@@ -97,15 +253,90 @@ export default {
       ],
       show: false,
       titleName: '',
+      isShowDialog: null,
+      addForm: {},
+      editForm: {
+        creatBy: 'hahah',
+        creatTime: 1628660777971,
+      },
+      detailForm: {},
+      usreList: [{}, {}, {}],
+      channelList: [],
     }
   },
   methods: {
+    formatDate,
     goBack() {
       this.$router.go(-1)
     },
-    addCode() {},
+    addCode() {
+      this.isShowDialog = 1
+      this.show = true
+      this.titleName = '新增企微活码'
+    },
+    changeUsre(val) {
+      console.log(val)
+    },
+    checkChange(val) {
+      console.log(val)
+    },
+    editBtn(v) {
+      console.log(v)
+      this.titleName = '编辑企微活码'
+      this.isShowDialog = 2
+      this.show = true
+    },
+    sendCode(item, index) {
+      console.log(item, index)
+      console.log(window.wx)
+      // wx.ready(function () {
+      //   // config信息验证后会执行ready方法，所有接口调用都必须在config接口获得结果之后，config是一个客户端的异步操作，所以如果需要在页面加载时就调用相关接口，则须把相关接口放在ready函数中调用来确保正确执行。对于用户触发时才调用的接口，则可以直接调用，不需要放在ready函数中。
+      //   // 扫一扫功能例子
+      //   wx.invoke({
+      //     desc: 'scanQRCode desc',
+      //     needResult: 0, // 默认为0，扫描结果由企业微信处理，1则直接返回扫描结果，
+      //     scanType: ['qrCode', 'barCode'], // 可以指定扫二维码还是条形码（一维码），默认二者都有
+      //     success: function (res) {
+      //       // 回调
+      //     },
+      //     error: function (res) {
+      //       if (res.errMsg.indexOf('function_not_exist') > 0) {
+      //         alert('版本过低请升级')
+      //       }
+      //     },
+      //   })
+      // })
+    },
+    checkDetail(item, index) {
+      this.titleName = '企微活码详情'
+      this.isShowDialog = 3
+      this.show = true
+    },
+    deleteBtn(v) {
+      this.$dialog
+        .confirm({
+          title: '温馨提示',
+          message: '删除后将不可恢复，是否确认删除？',
+          className: 'deleteBtn',
+          confirmButtonText: '是',
+          cancelButtonText: '否',
+          messageAlign: 'left',
+        })
+        .then(() => {
+          // on confirm
+        })
+        .catch(() => {
+          // on cancel
+        })
+    },
+    changeChannel() {},
     closeDialog() {},
-    saveDialog() {},
+    saveDialog(v) {
+      console.log(v)
+    },
+    fnChangeUser(val) {
+      console.log(val)
+    },
   },
 }
 </script>
@@ -293,13 +524,107 @@ export default {
         font-weight: 600;
       }
       .content {
-        // height: 845px;
-        padding: 24px;
+        height: 944px;
+        // padding: 24px;
+        .addForm,
+        .codeDetail {
+          height: 800px;
+          box-sizing: border-box;
+          padding: 24px;
+          font-size: 28px;
+          font-weight: 500;
+          overflow-y: scroll;
+          /deep/.el-form {
+            height: 100%;
+            .el-form-item {
+              display: flex;
+              margin-bottom: 26px;
+              .editText {
+                font-size: 28px;
+                line-height: 80px;
+              }
+            }
+            .el-form-item__label {
+              width: 155px;
+              font-size: 28px;
+              line-height: 80px;
+              color: #838a9d;
+            }
+            .el-form-item__content {
+              flex: 1;
+              height: 80px;
 
+              .el-input__inner {
+                height: 80px;
+                border-radius: 8px;
+                font-size: 28px;
+              }
+              .el-select,
+              .el-cascader {
+                width: 100%;
+                // width: 562px;
+                height: 80px;
+              }
+              .el-checkbox {
+                width: 100%;
+                line-height: 80px;
+
+                .el-checkbox__inner {
+                  width: 28px;
+                  height: 28px;
+                  &::after {
+                    width: 12px;
+                    height: 12px;
+                  }
+                }
+                .el-checkbox__input {
+                  vertical-align: -10%;
+                }
+                .el-checkbox__label {
+                  line-height: 80px;
+                  font-size: 28px;
+                }
+              }
+            }
+            .textareaInput {
+              height: 400px;
+              .el-textarea {
+                .el-textarea__inner {
+                  font-size: 28px;
+                  height: 400px;
+                }
+              }
+            }
+            .submitBtn {
+              .el-button--primary {
+                font-size: 28px;
+                width: 702px;
+                height: 80px;
+                background: #4168f6;
+                border-radius: 8px;
+              }
+            }
+          }
+        }
+        .codeDetail {
+          .el-form {
+            .codeImg {
+              margin-bottom: 100px;
+            }
+            span {
+              font-size: 28px;
+              line-height: 80px;
+            }
+          }
+        }
         .buttonWarp {
           display: flex;
-          justify-content: space-between;
+          justify-content: space-around;
           font-size: 28px;
+          width: 100%;
+          position: fixed;
+          bottom: 24px;
+
           span {
             display: inline-block;
             width: 339px;
