78d2d3e28732b603ba4939a4c7b847211099fff0
linshaojie <linshaojie@ruixin.tech>
2021-08-26 16:57:48 +0800

    delete all

--- scrm-app/src/views/customerManage/index.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div>
-    <keep-alive>
-      <router-view v-if="$route.meta.keepAlive"></router-view>
-    </keep-alive>
-    <router-view v-if="!$route.meta.keepAlive"></router-view>
-  </div>
-</template>
