dc29d67a6e1fcfcad47b3aae789200f432b787e2
linshaojie <linshaojie@ruixin.tech>
2021-08-27 14:39:41 +0800

    修改环境配置

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,10 @@
 import router from '../router/index'
 import axios from 'axios'
 let BASE_URL = ''
+    // console.log(location.hostname)
 if (location.hostname == 'localhost') {
     BASE_URL = 'http://172.10.7.114:8099' //袁
-} else if (location.hostname == 'console-scrm.ruixin.tech') {
+} else if (location.hostname == 'h5-scrm.ruixin.tech') {
     BASE_URL = 'https://api-scrm.ruixin.tech'
 } else if (location.hostname == 'dev-h5-console.jizhouhudong.com') {
     BASE_URL = 'dev-h5-console.jizhouhudong.com' //开发
