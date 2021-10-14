363cf183f31031158df887e37278606ea65fbe30
meilin <meilin@ruixin.tech>
2021-10-14 17:08:29 +0800

    update: 转发消息

--- src/views/talkTool/materialTemplate.vue
+++ src/views/talkTool/materialTemplate.vue
@@ -16,7 +16,7 @@
                     @load="onLoad"
                     >
                     <div class="article-item item" v-for="i in articleList" :key="i.articleId">
-                        <div class="left" @click="sendChatMessage('text', false, `${location.origin}/materialTemplate?materialId=${i.articleId}&type=1&userNo=${userNo}`)"><img src="../../images/relay.png" alt=""></div>
+                        <div class="left" @click="sendChatMessage('text', false, `${originUrl}/materialTemplate?materialId=${i.articleId}&type=1&userNo=${userNo}`)"><img src="../../images/relay.png" alt=""></div>
                         <div class="right">
                             <img class="img" :src="i.cover" alt="">
                             <div class="des">
@@ -36,7 +36,7 @@
                     @load="onLoad"
                     >
                     <div class="file-item item" v-for="i in saleList" :key="i.documentId">
-                        <div class="left" @click="sendChatMessage('text', false, `${location.origin}/materialTemplate?materialId=${i.documentId}&type=2&userNo=${userNo}`)"><img src="../../images/relay.png" alt=""></div>
+                        <div class="left" @click="sendChatMessage('text', false, `${originUrl}/materialTemplate?materialId=${i.documentId}&type=2&userNo=${userNo}`)"><img src="../../images/relay.png" alt=""></div>
                         <div class="right">
                             <img class="img" :src="i.cover" alt="">
                             <div class="des">
@@ -98,6 +98,8 @@ export default {
             posterListPage: 1,
             posterListLoading: false,
             posterListFinished: false,
+
+            originUrl: location.origin
         }
     },
     provide() {
