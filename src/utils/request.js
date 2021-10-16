742032cfa2447507e64d448ee314c96f6e9acf02
liuxiao <liuxiao247464@163.com>
2021-10-17 01:41:46 +0800

    修改bug

--- src/utils/request.js
+++ src/utils/request.js
@@ -8,9 +8,9 @@ let BASE_URL = ''
     // console.log(location.hostname)
 if (location.hostname == 'localhost') {
     // BASE_URL = 'http://172.10.7.114:8099/' //袁
-    BASE_URL = 'http://172.10.7.200:8099/' //刘
+    // BASE_URL = 'http://172.10.7.200:8099/' //刘
     // BASE_URL = 'https://dev-api.jzcrm.com' //开发
-    // BASE_URL = 'https://test-api.jzcrm.com' //测试
+    BASE_URL = 'https://test-api.jzcrm.com' //测试
         // BASE_URL = 'http://172.10.6.144:8099' // 李泉
     addVconsole()
 } else if (location.hostname == 'dev-h5.jzcrm.com') {
