01b8f3ecad4df4df363cfb32f87d1077c633beef
liuxiao <liuxiao247464@163.com>
2021-11-03 15:12:22 +0800

    lx

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -228,7 +228,7 @@ export default {
       duration: 1000,
       loadingType: "spinner",
     });
-    CommonHome.getWxToken();
+    // CommonHome.getWxToken();
   },
   mounted() {
     setTimeout(() => {
