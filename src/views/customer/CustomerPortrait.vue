a8653fc34f43ccee45765ad92f4ed5215650ceb2
meilin <meilin@ruixin.tech>
2021-11-04 16:18:34 +0800

    fix

--- src/views/customer/CustomerPortrait.vue
+++ src/views/customer/CustomerPortrait.vue
@@ -12,7 +12,7 @@
       <!-- 客户动态 -->
       <dynamic v-if="contentType == 0" ref="dynamic" :comeType="1" :isPortrait="1" :sendUserInfo="sendUserInfo" @fillMessage="getPeople"></dynamic>
 
-      <!-- 协作人 -->
+      <!-- 协助人 -->
       <div class="xiezuoBox" style="padding: 15px;" v-if="contentType == 1">
       	<HelperFile :isPortrait="1"></HelperFile>
       </div>
@@ -52,14 +52,14 @@
     <!-- 新手引导 -->
     <guide-box ref="guideBox"></guide-box>
 
-    <!-- 协作人消息输入框 -->
+    <!-- 协助人消息输入框 -->
     <message-box v-if="contentType == 0" ref="messageBox"></message-box>
 
-    <!-- 协作人选择弹窗 -->
+    <!-- 协助人选择弹窗 -->
     <reminders-box ref="remindersBox" :customerNo="item && item.clueCustomerNo"></reminders-box>
   </div>
   
-  <div v-else>
+  <div v-else-if="showPortraitType == 2">
     <Groupportrait></Groupportrait>
   </div>
 
@@ -121,13 +121,13 @@ export default {
   computed: {
     // ...mapState(['userId']),
     entry() {
-      return 'single_chat_tools'
+      // return 'single_chat_tools'
       // console.log("this.$store.getters.entry???", this.$store.getters.entry)
-      // return this.$store.getters.entry || sessionStorage.getItem('entry')
+      return this.$store.getters.entry || sessionStorage.getItem('entry')
     },
     userId() {
-      return 'wmY-gRDAAAeWi_4fz3ic79XYqKRG7EZg'
-      // return this.$store.getters.userId || sessionStorage.getItem('userId')
+      // return 'wmY-gRDAAAeWi_4fz3ic79XYqKRG7EZg'
+      return this.$store.getters.userId || sessionStorage.getItem('userId')
     },
   },
   watch: {
@@ -141,7 +141,7 @@ export default {
     }
   },
   created() {
-    // commonFun.getWxAppid()
+    commonFun.getWxAppid()
   },
   mounted() {
     window.onresize = () => (() => {
