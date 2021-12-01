95a70cf9f611c0dc38cdc04a3f10f37c4bef5f83
zhanminghao <zhanminghao@ruixin.tech>
2021-12-01 13:01:24 +0800

    fix

--- src/api/base.js
+++ src/api/base.js
@@ -2,8 +2,7 @@ import request from '@/utils/requestNew'
 let BASE_USER_SERVICE = 'user-service'
 export function user_getappid(data) { //获取appid
     return request({
-        url: BASE_USER_SERVICE + '/m/user/getcustomizeappid?redirect_uri=' + data,
-        // url: BASE_USER_SERVICE + '/m/user/getappid?redirect_uri=' + data,
+        url: BASE_USER_SERVICE + '/m/user/getappid?redirect_uri=' + data,
         method: 'get'
     })
 }
