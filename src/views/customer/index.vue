b3dba9fd28ff02509fe4ab43ee2cfe779a891b34
zhanminghao <zhanminghao@ruixin.tech>
2021-12-08 11:28:21 +0800

    fix

--- src/views/customer/index.vue
+++ src/views/customer/index.vue
@@ -14,6 +14,9 @@ export default {
         currentComponents(){
             let { comeFrom } = this.$route.query
             let val = process.env.NODE_ENV === 'development' || comeFrom == 'messageCard' ? 'single_chat_tools' : this.$store.getters.entry
+            if(!val){
+                val = 'single_chat_tools'
+            }
             console.log('obj val',val)
             let obj = {
                 'single_chat_tools': CustomerPortrait,
