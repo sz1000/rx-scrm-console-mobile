900c62c2ea348df7dc5ebb6029d4e0860d3dc6fb
linshaojie <linshaojie@ruixin.tech>
2021-12-10 23:00:31 +0800

    lsj

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -74,7 +74,7 @@
       <div class="btnRouter custom">
         <div class="commonBtn" @click="FnToRouter('/talkTool/contentMaterial')" v-show="menulist.includes('material')">
           <img src="../images/neirongsucai.png" alt="" />
-          <span>内容素材</span>
+          <span>素材管理</span>
         </div>
         <div class="commonBtn" @click="FnToRouter('/talkTool/verbalTrickConsole')" v-show="menulist.includes('verbalTrick')">
           <img src="../images/huashuku.png" alt="" />
