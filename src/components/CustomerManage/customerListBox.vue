9512cba6e15682677103e8ec9266f39c9f9a145b
meilin <meilin@ruixin.tech>
2021-12-13 19:03:30 +0800

    feat: 我的客户1

---
+++ src/components/CustomerManage/customerListBox.vue
@@ -0,0 +1,63 @@
+<template>
+  <ul class="customer-list-box">
+    <!-- <li v-for="i in listData" :key="i.id"> -->
+    <li class="list-item">
+      <div class="list-item-left">
+        <img src="@/assets/svg/avatar_default.svg" alt="">
+      </div>
+      <ul class="list-item-right">
+        <li class="right-top">
+          <div class="name-box">
+            <h3>黄少天</h3>
+            <span>@企业</span>
+          </div>
+          <span class="time">2021.10.13</span>
+        </li>
+        <li class="right-middle">
+          <span>沟通洽谈</span>
+          <span>活动推广</span>
+          <span>活动推广</span>
+          <span>活动推广</span>
+          <span>活动推广</span>
+          <span>活动推广</span>
+          <span>活动推广</span>
+          <span>活动推广</span>
+          <span>活动推广</span>
+          <span>活动推广</span>
+          <span>活动推广</span>
+        </li>
+        <li class="right-bottom">
+          <span></span>
+        </li>
+      </ul>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  name: 'customerListBox',
+  props: {
+    type: {
+      default: 0,
+    },
+    listData: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  },
+  inject: ['goDetail'],
+  methods: {
+
+  },
+}
+</script>
+<style lang="less" scoped>
+@import url('../../styles/color');
+.customer-list-box {
+  .list-item {
+    
+  }
+}
+</style>
