5554bd0f5ba3943232ac64fd98afe6d63ccc4f63
liuxiao <liuxiao247464@163.com>
2021-12-29 21:05:32 +0800

    lx

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,11 +7,11 @@ import VConsole from 'vconsole'
 let BASE_URL = ''
     // console.log(location.hostname)
 if (location.hostname == 'localhost') {
-    BASE_URL = 'http://172.10.6.177:8099' //葛
-        // BASE_URL = 'http://172.10.7.114:8099/' //袁
-        // BASE_URL = 'http://172.10.7.200:8099/' //刘
-        // BASE_URL = 'http://172.10.6.144:8099' // 李泉
-        // BASE_URL = 'https://test-api.jzcrm.com' //测试
+    // BASE_URL = 'http://172.10.6.177:8099' //葛
+    // BASE_URL = 'http://172.10.7.114:8099/' //袁
+    // BASE_URL = 'http://172.10.7.200:8099/' //刘
+    // BASE_URL = 'http://172.10.6.144:8099' // 李泉
+    BASE_URL = 'https://test-api.jzcrm.com' //测试
         // BASE_URL = 'http://172.10.7.170:8099/' //稳
         // BASE_URL = 'https://dev-api.jzcrm.com' //开发
     addVconsole()
